76c4e3d12a638f1d566279453341b5dc8ec3cd8e
FelipeDeMayo <felipemayobister@gmail.com>
2025-06-23 19:10:17 -0300

    Ajustando novas rotas dos produtos.

--- frontend/src/services/productService.ts
+++ frontend/src/services/productService.ts
@@ -9,13 +9,12 @@ const api = axios.create({
   }
 })
 
-
 export const getAllProducts = async () => {
-  const response = await api.get('/api/products')
+  const response = await api.get('/products')
   return response.data
 }
 
 export const getProductById = async (id: number) => {
-  const response = await api.get(`/api/products/${id}`)
+  const response = await api.get(`/products/${id}`) 
   return response.data
 }
